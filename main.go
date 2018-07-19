2ca7257a8f258425c9cd7b12a56e9f44e4254e8b
hagna <natester@gmail.com>
2018-07-19 10:39:10 -0600

    Watch a command over ssh and report to gravwell.

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/entry"
+	flag "github.com/spf13/pflag"
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"path/filepath"
 	"strings"
-
-	flag "github.com/spf13/pflag"
-	"golang.org/x/crypto/ssh"
 	"time"
 )
 
 var password = flag.StringP("password", "p", "words!@#", "Password")
+var TAGNAME = flag.StringP("tagname", "t", "word", "gravwell tag name")
+var SECRET = flag.StringP("secret", "s", "asdfasdf", "gravwell secret")
 var keyfilename = flag.String("keyfile", os.Getenv("HOME")+"/.ssh/id_rsa", "key file")
 var prog = os.Args[0]
 var docu = "[user@]hostname[:port] \"command\""
@@ -23,6 +29,15 @@ var WAIT = flag.DurationP("interval", "n", time.Second*2, "Number of seconds to 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s: %s [options] %s\n", prog, prog, docu)
 	flag.PrintDefaults()
+}
+
+func initIngest(tagname, isecret string) (*ingest.IngestConnection, error) {
+
+	igst, err := ingest.InitializeConnection("tcp://127.0.0.1:4023", isecret, []string{tagname}, "", "", false)
+	if err != nil {
+		return nil, err
+	}
+	return igst, err
 }
 
 func main() {
@@ -75,19 +90,47 @@ func main() {
 	// connect
 	client, err := ssh.Dial("tcp", host+":"+port, config)
 	if err != nil {
-		log.Println("Error on ssh.Dial")
 		log.Fatal(err)
 	}
 	defer client.Close()
+
+	igst, err := initIngest(*TAGNAME, *SECRET)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer igst.Close()
+	r, w := io.Pipe()
+	tagid, ok := igst.GetTag(*TAGNAME)
+	if !ok {
+		log.Fatal("couldn't look up tag")
+	}
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			// Now we'll create an Entry
+			ent := entry.Entry{
+				TS:   entry.Now(),
+				SRC:  net.ParseIP("127.0.0.1"),
+				Tag:  tagid,
+				Data: []byte(s.Text()),
+			}
+
+			// And finally write the Entry
+			err := igst.WriteEntrySync(&ent)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println("SENT " + s.Text())
+			}
+		}
+	}()
 	for {
 		sess, err := client.NewSession()
 		if err != nil {
-			log.Println("error on NEwSession()")
 			log.Fatal(err)
 		}
-		// setup standard out and error
-		// uses writer interface
-		sess.Stdout = os.Stdout
+		sess.Stdout = w
 		sess.Stderr = os.Stderr
 
 		err = sess.Run(cmd)
